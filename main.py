088a369db201f06977283c2fd9a0c65bf1675b18
Daniel Gabriel <HARRY2@MacBook-Air.local>
2021-11-07 01:34:04 -0500

    Fix

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from website import create_app
-from gevent.pywsgi import WSGIServer
+#from gevent.pywsgi import WSGIServer
 
 app = create_app()
 
-http_server = WSGIServer(('', 8000), app)
-http_server.serve_forever()
-# if __name__ == "__main__":
-#     app.run(debug=True)
+# http_server = WSGIServer(('', 8000), app)
+# http_server.serve_forever()
+if __name__ == "__main__":
+    app.run(debug=True)
